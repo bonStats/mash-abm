37fdfa7c8a404a69629b26ce96c9087a36f87458
Joshua Bon <joshuajbon@gmail.com>
2025-01-07 09:49:38 +0100

    Spelling

--- docs/worksheets/scripts/worksheet1.R
+++ docs/worksheets/scripts/worksheet1.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(matrixStats)
 
 josh_data_dir <- function(fl){
   paste0("/Users/jbon/github/mash-abm/data/", fl)
@@ -50,22 +51,26 @@ qgamma(1-0.025, shape = m1_alpha, rate = m1_beta))
 
 # Model evidence 1
 
-val_lambda <- 3.5
+
 post_logdens1 <- function(lambda) dgamma(lambda, shape = m1_alpha, rate = m1_beta, log = T)
 prior_logdens1 <- function(lambda) dgamma(lambda, shape = alpha0, rate = beta0, log = T)
-loglikelihood1 <- function(lambda, y) sum(dpois(y, lambda = lambda, log = T)) # Don't use, too small value
+loglikelihood1 <- function(lambda, y) sum(dpois(y, lambda = lambda, log = T)) # Don't use for MC, too small value
 loglikelihoodC1 <- function(lambda, y) sum(y*log(lambda) - lambda)
 
 
 log_model_evidence1 <-function(y, include_factorial = T){ # includes factorial term from likelihood by default
   
-  ll <- ifelse(include_factorial, loglikelihood1(val_lambda, y) , loglikelihoodC1(val_lambda, y))
+  val_lambda <- 4 # dummy value, doesn't matter
+  
+  ll <- ifelse(include_factorial, 
+               loglikelihood1(val_lambda, y) , 
+               loglikelihoodC1(val_lambda, y)
+               )
   prior_logdens1(val_lambda) + ll - post_logdens1(val_lambda)
   
 }
 
-log_model_evidence1(deputes$questions_orales)
-log_model_evidence1(deputes$questions_orales, include_factorial = F)
+log_model_evidence1(y, include_factorial = T)
 
 # Model 2: Gamma prior
 
@@ -97,86 +102,70 @@ c(qgamma(0.025, shape = m2_alpha2, rate = m2_beta2),
   qgamma(1-0.025, shape = m2_alpha2, rate = m2_beta2))
 
 
-# Model evidence 1
+# Model evidence 2
 
-val_lambda1 <- 3.5
-val_lambda2 <- 3
-post_logdens2 <- function(lambda1,lambda2) dgamma(lambda1, shape = m2_alpha1, rate = m2_beta1, log = T) + dgamma(lambda2, shape = m2_alpha2, rate = m2_beta2, log = T)
+post_logdens2 <- function(lambda1,lambda2){
+  dgamma(lambda1, shape = m2_alpha1, rate = m2_beta1, log = T) + 
+    dgamma(lambda2, shape = m2_alpha2, rate = m2_beta2, log = T)
+  }
 prior_logdens2 <- function(lambda1,lambda2) dgamma(lambda1, shape = alpha0, rate = beta0, log = T) + dgamma(lambda2, shape = alpha0, rate = beta0, log = T)
-loglikelihood2 <- function(lambda1,lambda2, y1,y2) sum(dpois(y1,lambda1)) + sum(dpois(y2,lambda2))
+loglikelihood2 <- function(lambda1,lambda2, y1,y2) sum(dpois(y1,lambda1,log=T)) + sum(dpois(y2,lambda2,log=T))
 loglikelihoodC2 <- function(lambda1,lambda2, y1,y2) sum(y1*log(lambda1) - lambda1) + sum(y2*log(lambda2) - lambda2)
 
 log_model_evidence2 <- function(y1,y2, include_factorial = T){
   
-  ll <- ifelse(include_factorial, loglikelihood2(val_lambda1, val_lambda2, y1, y2), loglikelihoodC2(val_lambda1, val_lambda2, y1, y2))
+  val_lambda1 <- 3 # dummy values, doesn't effect computation
+  val_lambda2 <- 3
+  
+  ll <- ifelse(include_factorial, 
+               loglikelihood2(val_lambda1, val_lambda2, y1, y2), 
+               loglikelihoodC2(val_lambda1, val_lambda2, y1, y2)
+               )
   prior_logdens2(val_lambda1, val_lambda2) + ll - post_logdens2(val_lambda1, val_lambda2)
   
 }
 
-log_model_evidence2(y1, y2)
 log_model_evidence2(y1, y2, include_factorial = F)
 
-# log evidence excluding product of factorials from likelihood
+# BF: M2 vs M1
 
-log_model_evidence2(y1, y2, include_factorial = T) -
+log_BF21 <- log_model_evidence2(y1, y2, include_factorial = T) -
   log_model_evidence1(y, include_factorial = T)
+log_BF21
+
+# log evidence excluding product of factorials from likelihood
+exp(log_model_evidence2(y1, y2, include_factorial = F) -
+  log_model_evidence1(y, include_factorial = F))
+exp(log_BF21)
 
 
-# BF:
+# MC estimation
 
-log_model_evidence2(filter(deputes,sexe=="H")$questions_orales, filter(deputes,sexe=="F")$questions_orales) - 
-
-# Model 1: Jeffery's prior
-
-ybar <- mean(deputes$questions_orales)
-n <- nrow(deputes)
-
-# posterior
-m1_alpha <- n * ybar + 0.5
-m1_beta <- n
+N <- 1000
+smp_prior1 <- rgamma(N, shape = alpha0, rate = beta0)
+smp_prior2_1 <- rgamma(N, shape = alpha0, rate = beta0)
+smp_prior2_2 <- rgamma(N, shape = alpha0, rate = beta0)
 
 
-# Model 2: Jeffery's prior
-
-ybar1 <- mean(filter(deputes,sexe=="H")$questions_orales) # H
-ybar2 <- mean(filter(deputes,sexe=="F")$questions_orales) # F
+vec_loglikelihoodC1 <- Vectorize(loglikelihoodC1, vectorize.args = "lambda")
+vec_loglikelihoodC2 <- Vectorize(loglikelihoodC2, vectorize.args = c("lambda1","lambda2"))
 
 
-# posterior
-m2_alpha1 <- n * ybar1 + 0.5
-m2_alpha2 <- n * ybar2 + 0.5
-m2_beta1 <- n
-m2_beta2 <- n
+mc_log_me1_C <- vec_loglikelihoodC1(smp_prior1, y)
+mc_log_me2_C <- vec_loglikelihoodC2(smp_prior2_1, smp_prior2_2, y1, y2)
+
+# might be numerically unstable
+mean(exp(mc_log_me2_C)) / mean(exp(mc_log_me1_C))
+
+# numerically stable
+exp(logSumExp(mc_log_me2_C) - logSumExp(mc_log_me1_C)) # logSumExp from matrixStats package
+
+# true value
+exp(log_BF21)
 
 
-# Monte carlo approximation
 
-# r_lambda = lambda_1 / lambda_2
+# Model 1/2: Jeffery's prior
 
-N <- 100
-
-# posterior
-samples_lambda_1 <- rgamma(N, m2_alpha1, rate = m2_beta1)
-samples_lambda_2 <- rgamma(N, m2_alpha2, rate = m2_beta2)
-
-r_lambda <- samples_lambda_1 / samples_lambda_2
-mean(r_lambda)
-
-
-hist(r_lambda)
-
-mean(r_lambda)
-sqrt(var(r_lambda))
-
-# prior 
-samples_lambda_1 <- rgamma(N, alpha0, rate = beta0)
-samples_lambda_2 <- rgamma(N, alpha0, rate = beta0)
-
-r_lambda <- samples_lambda_1 / samples_lambda_2
-
-hist(r_lambda)
-
-mean(r_lambda)
-sqrt(var(r_lambda))
-
-
+# repeat previous code with alpha = 1/2 and beta0 = 0
+# this is equivalnt to Jeffery's prior
